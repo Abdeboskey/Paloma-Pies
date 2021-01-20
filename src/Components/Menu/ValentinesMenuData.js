a83338c5e2d7df3b30552cedda447e045602a6f2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-19 19:47:49 -0700

    Rename menudata to valentines menudata and add example for chenoa

---
+++ src/Components/Menu/ValentinesMenuData.js
@@ -0,0 +1,30 @@
+const valentinesMenu = [
+  {
+   name: 'Chocolate Creme',
+   ingredients: {
+      filling: 'it’s made of love',
+      crust: 'It’s made of crispy flakey love It’s made of crispy flakey love',
+    } 
+  },
+  {
+   name: 'Chocolate Creme',
+   ingredients: {
+      filling: 'it’s made of love',
+      crust: 'It’s made of crispy flakey love'
+    } 
+  },
+  {
+   name: 'Chocolate Creme',
+   ingredients: {
+      filling: 'it’s made of love',
+      crust: 'It’s made of crispy flakey love'
+    } 
+  },
+  {
+   name: 'Chocolate Creme',
+   ingredients: {
+      filling: 'it’s made of love',
+      crust: 'It’s made of crispy flakey love'
+    } 
+  },
+]
