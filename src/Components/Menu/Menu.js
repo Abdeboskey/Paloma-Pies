dbd1e6ff7af49bc789bc3409339830a1110f03f9
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-19 19:47:12 -0700

    Create example function for chenoa

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Menu = ({menuData}) => {
+  // this function takes in a menu, and returns a collection of pie items
+  menuData.forEAch(pie => {
+     //Make a Pie!
+  })
+}
+
+export default Menu
