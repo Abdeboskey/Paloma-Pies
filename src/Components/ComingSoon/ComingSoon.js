bb7ac6cad8dd5e5c03b88d57ffb5ff79222d8cc7
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-24 21:01:19 -0700

    Add full sized image as comingsoon background #4

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import comingSoonBanner from '../../assets/images/comingSoonBanner.jpg'
+import IMG_7839 from '../../assets/images/IMG_7839.jpg'
 
 const ComingSoon = () => {
   return (
     <main>
       <h1>Welcome to Paloma Pies!<span>🕊</span></h1>
       <p>This page is currently under construction in preparation for our Valentine's Day Sale! Please check back on Monday, February 1st to see our new menu and place an order!</p>
-      <img src={comingSoonBanner} alt="creme pie with sliced fruit and sunlight" style={{ width: '100%' }}/>
+      <img src={IMG_7839} alt="creme pie with sliced fruit and sunlight" style={{ width: '100%' }}/>
     </main>
   )
 }
