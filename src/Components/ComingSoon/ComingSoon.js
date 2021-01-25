a5c6e1eaaac55a784e39b8ae383fbd45d143dd8c
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-24 20:45:47 -0700

    Update copy and image style #4

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -1,12 +1,12 @@
 import React from 'react'
-const comingSoonBanner = require('../../assets/images/comingSoonBanner.jpg')
+import comingSoonBanner from '../../assets/images/comingSoonBanner.jpg'
 
 const ComingSoon = () => {
   return (
     <main>
       <h1>Welcome to Paloma Pies!<span>🕊</span></h1>
-      <p>This page is currently under construction in preparation for our Valentie's Day Sale! Please check back on Monday, February 1st to see our new menu and place an order!</p>
-      <img src={comingSoonBanner} alt="creme pie with sliced fruit and sunlight" />
+      <p>This page is currently under construction in preparation for our Valentine's Day Sale! Please check back on Monday, February 1st to see our new menu and place an order!</p>
+      <img src={comingSoonBanner} alt="creme pie with sliced fruit and sunlight" style={{ width: '100%' }}/>
     </main>
   )
 }
