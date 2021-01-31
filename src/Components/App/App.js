c12d7b69fb02aabbf265ffa884c4139f1b21b3b2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-31 00:46:26 -0700

    Add vdaybanner component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import Landing from '../Landing/Landing'
+import VDayBanner from '../VDayBanner/VDayBanner'
 import './App.scss'
 
 function App() {
   return (
     <div>
       <Landing />
+      <VDayBanner />
     </div>
   )
 }
