d5f4eaf4b0fc2bfb1a9b2b0372cbe39add1d81ba
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-31 17:25:30 -0700

    Create menu header and background

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,30 +1,37 @@
 import React from 'react'
-import Pie from '../Pie/Pie'
+// import Pie from '../Pie/Pie'
+import style from './Menu.module.scss'
+import zigZagBanner from '../../assets/images/zigZagBanner.png'
 
 const Menu = (props) => {
   const { menuData } = props
 
   const makePies = () => {
-    return menuData.map(pie => {
-      const {
-        name,
-        ingredients,
-      } = pie
+    // return menuData.map(pie => {
+    //   const {
+    //     name,
+    //     ingredients,
+    //   } = pie
 
-      return (
-        <Pie 
-          name={name}
-          filling={ingredients.filling}
-          crust={ingredients.crust}
-        />
-      )
-    })
+    //   return (
+    //     <Pie 
+    //       name={name}
+    //       filling={ingredients.filling}
+    //       crust={ingredients.crust}
+    //     />
+    //   )
+    // })
   }
   
   return (
-    <section>
+    <div className={style.Menu}>
+      <img className={`img-fluid ${style.zigZig}`} src={zigZagBanner} alt="hand drawn zig zag design"/>
+      <div className={style.menuCard}>
+        <h1 className={`${style.heading}`}>Menu</h1>
+      </div>
+      <div className={`${style.menuSection}`}></div>
       {makePies}
-    </section>
+    </div>
   )
 }
 
