36c7b0f7489e96a27583f47becaf1ee5c7ec8a92
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-01-31 18:28:44 -0700

    Create pie pan

---
+++ src/Components/Pie/Pie.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import style from './Pie.module.scss'
+
+const Pie = (props) => {
+const {
+  name,
+  description,
+  ingredients,
+  img1,
+  img2, 
+  availableGF,
+  availableVegan
+} = props
+
+  return (
+    <div className={style.Pie}>
+
+    </div>
+
+  )
+}
+
+export default Pie
