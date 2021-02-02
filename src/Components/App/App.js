8ff05e4f29183632a6eb35d36212d1b87a33ffbd
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-02 13:25:55 -0700

    Add footer to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import Landing from '../Landing/Landing'
 import VDayBanner from '../VDayBanner/VDayBanner'
 import Menu from '../Menu/Menu'
 import './App.scss'
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Landing />
       <VDayBanner />
       <Menu />
+      <Footer />
     </div>
   )
 }
