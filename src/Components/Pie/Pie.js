e626905903fededa656089c5fa9717291e374356
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-02 13:27:26 -0700

    Remove link on pies

--- src/Components/Pie/Pie.js
+++ src/Components/Pie/Pie.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import style from './Pie.module.scss'
 
-const orderForm = 'https://docs.google.com/forms/d/e/1FAIpQLSeArz3bQT3gvbQ_PCHv4p3nr5-g6cZnQTAxkWlhl7BttINSLA/viewform'
-
 const Pie = (props) => {
 const {
   id,
@@ -19,20 +17,18 @@ const {
         src={img1}
         alt={`${name} on table with plants and sunlight`}
       />
-      <a href={orderForm} target="_blank" rel="noreferrer">
-        <div className={style.pieDetail}>
-          <div className={style.film}></div>
-          <h2 className={id % 2 === 0 ? style.pieTitleRight : style.pieTitleLeft}>
-            {name}
-          </h2>
-          <p className={id % 2 === 0 ? style.descLeft : style.descRight}>“{description}”</p>
-          <img
-            className={style.piePic}
-            src={img2}
-            alt={`slice of ${name}`}
-          />
-        </div>
-      </a>
+      <div className={style.pieDetail}>
+        <div className={style.film}></div>
+        <h2 className={id % 2 === 0 ? style.pieTitleRight : style.pieTitleLeft}>
+          {name}
+        </h2>
+        <p className={id % 2 === 0 ? style.descLeft : style.descRight}>“{description}”</p>
+        <img
+          className={style.piePic}
+          src={img2}
+          alt={`slice of ${name}`}
+        />
+      </div>
     </div>
   );
 }
