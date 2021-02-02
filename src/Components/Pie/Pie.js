32551e857484eda792e6caecc546c738d7ccb318
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-01 21:13:01 -0700

    Add detail menu to pie

--- src/Components/Pie/Pie.js
+++ src/Components/Pie/Pie.js
@@ -6,19 +6,31 @@ const {
   id,
   name,
   description,
-  ingredients,
   img1,
-  img2, 
-  availableGF,
-  availableVegan
+  img2,
 } = props
 
   return (
     <div className={style.Pie} key={id}>
-      {/* <h2 className={id % 2 === 0 ? style.pieTitleRight : style.pieTitleLeft}>{name}</h2> */}
-      <img className={style.piePic} src={img1} alt={`${name} on table with plants and sunlight`}/>
+      <img
+        className={style.piePic}
+        src={img1}
+        alt={`${name} on table with plants and sunlight`}
+      />
+      <div className={style.pieDetail}>
+        <div className={style.film}></div>
+        <h2 className={id % 2 === 0 ? style.pieTitleRight : style.pieTitleLeft}>
+          {name}
+        </h2>
+        <p className={id % 2 === 0 ? style.descLeft : style.descRight}>“{description}”</p>
+        <img
+          className={style.piePic}
+          src={img2}
+          alt={`slice of ${name}`}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Pie
+export default Pie 
