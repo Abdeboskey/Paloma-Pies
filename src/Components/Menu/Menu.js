3764e3f9adcf9dc34ee2b5720e1e761cc6c88667
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-08 20:31:08 -0700

    Move orderbutton out of footer into menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -4,6 +4,9 @@ import valentinesMenu from './ValentinesMenuData.js'
 import style from './Menu.module.scss'
 import zigZagBanner from '../../assets/images/zigZagBanner.png'
 
+const orderForm = 'https://docs.google.com/forms/d/e/1FAIpQLSeArz3bQT3gvbQ_PCHv4p3nr5-g6cZnQTAxkWlhl7BttINSLA/viewform'
+
+
 const Menu = () => {
 
   const makePies = () => {
@@ -47,7 +50,11 @@ const Menu = () => {
   return (
     <div className={style.Menu}>
       <div className={style.banner}>
-        <img className={`img-fluid ${style.zigZag}`} src={zigZagBanner} alt="hand drawn zig zag design"/>
+        <img
+          className={`img-fluid ${style.zigZag}`}
+          src={zigZagBanner}
+          alt="hand drawn zig zag design"
+        />
         <div className={style.menuCard}>
           <h1 className={`${style.heading}`}>Menu</h1>
         </div>
@@ -55,8 +62,23 @@ const Menu = () => {
       <div className="d-flex flex-wrap justify-content-center mt-2">
         {makePies()}
       </div>
+      <div className={style.orderBanner}>
+        <img
+          className={`img-fluid ${style.zigZag}`}
+          src={zigZagBanner}
+          alt="hand drawn zig zag design"
+        />
+        <a
+          className={style.orderLink}
+          href={orderForm}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h2>Click Here To Place Your Order</h2>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Menu
