9b4f2ee8b60053d6cccc315829acc870d0b66365
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-09 13:01:22 -0700

    Adjust test text

--- src/Components/ClosedForBusiness/ClosedForBusiness.test.js
+++ src/Components/ClosedForBusiness/ClosedForBusiness.test.js
@@ -13,7 +13,7 @@ describe('ClosedForBusiness', () => {
 
     const ohHi = getByRole('heading', { name: /oh, hi!/i})
     const p1 = getByText(/paloma pies is all about handmade pies and treats\. everything is made from scratch in denver, co!/i)
-    const p2 = getByText("Ordering for the Valentine's day sale has closed - please check back for our next sale (Mother's Day 2021) and reach out if you have any questions!")
+    const p2 = getByText("Ordering for the Valentine's day sale has closed 😔 Please check back for our next sale (Mother's Day 2021) and reach out if you have any questions!")
 
     expect(ohHi).toBeInTheDocument()
     expect(p1).toBeInTheDocument()
