62492bf0459564c0bd9b346ee49a3629e87f5655
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-10 22:00:48 -0700

    Create classname wavyline

--- src/Components/WavyLine/WavyLine.js
+++ src/Components/WavyLine/WavyLine.js
@@ -3,7 +3,7 @@ import style from './WavyLine.module.scss'
 
 const WavyLine = () => {
   return (
-    <div className="d-flex justify-content-center">
+    <div className={`d-flex justify-content-center ${style.WavyLine}`}>
       <div className={style.downWave}></div>
       <div className={style.upWave}></div>
       <div className={style.downWave}></div>
