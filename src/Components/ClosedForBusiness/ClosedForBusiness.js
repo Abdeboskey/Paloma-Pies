8a7afa297d41aac80c13d2682ed4d517bfc67693
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-02-19 23:32:43 -0700

    Edit text

--- src/Components/ClosedForBusiness/ClosedForBusiness.js
+++ src/Components/ClosedForBusiness/ClosedForBusiness.js
@@ -8,13 +8,14 @@ const ClosedForBusiness = () => {
         <h2 className={`col-md-6 row-sm p-3 ${style.ohHi}`}>Oh, Hi!</h2>
         <p className={` col-md-4 p-3 align-self-center ${style.p}`}>
           Paloma Pies is all about handmade pies and treats. Everything is made
-          from scratch in Denver, CO!
+          from scratch in Denver, CO.
         </p>
       </div>
       <p className={`col-md-6 mx-auto m-5 ${style.p}`}>
-        Ordering for the Valentine's day sale has closed 😔 Please check back for
-        our next sale (Mother's Day 2021) and reach out if you have any
-        questions!
+        ⪦ The pie shop is currently closed ⪧<br />
+        Our next sale will be for Mother's Day 2021, 
+        and please <a href="mailto: info@palomapies.com">reach out</a> if you have any
+        questions or need a special order in the meanwhile ☺️
       </p>
     </div>
   );
