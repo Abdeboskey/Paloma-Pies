faeab8673c3d57bdc660ed92fe103225ea639796
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-03-27 21:02:55 -0600

    Update text in tests

--- src/Components/ClosedForBusiness/ClosedForBusiness.test.js
+++ src/Components/ClosedForBusiness/ClosedForBusiness.test.js
@@ -12,8 +12,8 @@ describe('ClosedForBusiness', () => {
     )
 
     const ohHi = getByRole('heading', { name: /oh, hi!/i})
-    const p1 = getByText(/paloma pies is all about handmade pies and treats\. everything is made from scratch in denver, co!/i)
-    const p2 = getByText("Ordering for the Valentine's day sale has closed 😔 Please check back for our next sale (Mother's Day 2021) and reach out if you have any questions!")
+    const p1 = getByText(/paloma pies is all about handmade pies and treats\. everything is made from scratch in denver, co/i)
+    const p2 = getByText(/the pie shop is currently closed/i);
 
     expect(ohHi).toBeInTheDocument()
     expect(p1).toBeInTheDocument()
