c71d7ca88042d83e723ed5a07ac8561739bcaf21
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-03-27 22:47:55 -0600

    Add router and initial routes for home and 404

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import Landing from '../Landing/Landing'
 import Footer from '../Footer/Footer'
 import ClosedForBusiness from '../ClosedForBusiness/ClosedForBusiness'
+import NotFound from '../NotFound/NotFound'
 import style from './App.module.scss'
 
 import palomaStamp from '../../assets/images/palomaStamp.png'
@@ -27,11 +33,20 @@ function App() {
           />
         </div>
       )}
-      <div className={loading ? style.hidden : style.main}>
-        <Landing />
-        <ClosedForBusiness />
-        <Footer />
-      </div>
+      <Router>
+        <div className={loading ? style.hidden : style.main}>
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+              <ClosedForBusiness />
+              <Footer />
+            </Route>
+            <Route>
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
